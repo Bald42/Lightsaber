6fd63e590993293d1f4ec0bad5316d938f2165f6
Bald42 <bald.andrushin@gmail.com>
2020-03-19 13:13:31 +0700

    Первая заливка

---
+++ Assets/GameResources/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using Photon.Realtime;
+
+/// <summary>
+/// Игровой менеджер
+/// </summary>
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    private const string NAME_SCENE_MENU = "Menu";
+
+    private const string NAME_PREFAB_PLAYER = "Player";
+
+    private void Start()
+    {
+        SpawnPlayer();
+    }
+
+
+    private void SpawnPlayer ()
+    {
+        PhotonNetwork.Instantiate(NAME_PREFAB_PLAYER, Vector3.zero, Quaternion.identity);
+    }
+
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+        Debug.Log($"Left room");
+        SceneManager.LoadScene(NAME_SCENE_MENU);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        base.OnPlayerEnteredRoom(newPlayer);
+        Debug.Log($"Player {newPlayer.NickName} entered room");
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        base.OnPlayerLeftRoom(otherPlayer);
+        Debug.Log($"Player {otherPlayer.NickName} left room");
+    }
+
+    /// <summary>
+    /// Покинуть комнату
+    /// </summary>
+    public void OnLeave ()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+}
---
+++ Assets/GameResources/Scripts/Rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Вращение
+/// </summary>
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 angleRotate = Vector3.zero;
+
+    [SerializeField]
+    private float speed = 0f;
+
+    private void Update()
+    {
+        DoRotate();
+    }
+
+    private void DoRotate ()
+    {
+        transform.Rotate(angleRotate * speed);
+    }
+}
